1aa362642e8019cd1bdfb299d89967699d58a247
Aayyuurr <abbasyanis4@gmail.com>
2023-05-27 22:22:08 +0200

    nav bar mobile

--- src/template/navbar.svelte
+++ src/template/navbar.svelte
@@ -6,32 +6,45 @@
 </script>
 
 <div class="nav-wrapper">
-	<button on:click={toggle}
-		><span>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 448 512"
-				><path
-					d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"
-				/></svg
-			>
-		</span></button
-	>
+	{#if !open}
+		<button on:click={toggle}
+			><span>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 448 512"
+					><path
+						d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"
+					/></svg
+				>
+			</span></button
+		>
+	{:else}
+		<button on:click={toggle}
+			><span>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 384 512"
+					><path
+						d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z"
+					/></svg
+				>
+			</span></button
+		>
+	{/if}
 	<a href="/">
-
 		<h1>Algerien Learn</h1>
 	</a>
 	{#if open}
-	<nav>
-		<ul>
-			<li><a href="/cours">cours</a></li>
-			<li><a href="/path">parcours</a></li>
-			<li><a href="/workshops">workshops</a></li>
-			<li><a href="tarif">trafication</a></li>
-		</ul>
-	</nav>
+		<nav>
+			<ul>
+				<li><a href="/cours">cours</a></li>
+				<li><a href="/path">parcours</a></li>
+				<li><a href="/workshops">workshops</a></li>
+				<li><a href="tarif">trafication</a></li>
+			</ul>
+		</nav>
 	{/if}
-	<div >
+	<div>
 		<button
 			><span
 				><svg
@@ -67,9 +80,25 @@
 		justify-content: space-around;
 
 		nav {
-			display: none;
+			position: fixed;
+			top: 68px;
+			left: 0;
+			width: 94%;
+			background: var(--bg-color);
+			height: 100vh;
+
+			padding-top: 5rem;
+			align-items: center;
+			border-right: 1px solid var(--black-color);
+			text-transform: uppercase;
+
+			text-justify: left;
+			ul li {
+				padding-left: 4rem;
+				margin-bottom: 1rem;
+			}
 		}
-		
+
 		button {
 			svg {
 				width: 20px;
@@ -84,20 +113,19 @@
 			height: 20px;
 			background-color: transparent;
 			border: none;
-			transition: transform .2s ease,opacity .2s ease;
+			transition: transform 0.2s ease, opacity 0.2s ease;
 			&:hover {
 				cursor: pointer;
-				// shrink 
+				// shrink
 				transform: scale(1.1);
 			}
 		}
 		div button:first-of-type {
 			margin-right: 10px;
 		}
-		div{
+		div {
 			display: flex;
 			align-items: center;
 		}
 	}
-	
 </style>
